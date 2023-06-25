a571ef25d4241e50890c902341461e62d487bfcd
ugm2 <unaigaraymaestre@gmail.com>
2023-06-25 21:30:43 +0200

    Remove prediction from training script
    Signed-off-by: ugm2 <unaigaraymaestre@gmail.com>

--- 04-deployment/batch/taxi_predictor/training.py
+++ 04-deployment/batch/taxi_predictor/training.py
@@ -29,49 +29,8 @@ def run_training(
     print(predictor.model_uri)
 
 
-def run_prediction(
-    predictor,
-    year,
-    month,
-    taxi_type,
-    model_version,
-    bucket_name,
-):
-    # Load the model using the model_uri
-    predictor.load_model(predictor.model_uri)
-
-    # Single ride prediction
-    single_ride = {"PULocationID": "10", "DOLocationID": "50", "trip_distance": 40}
-    single_duration = predictor.predict(single_ride)
-    print(
-        f"The predicted duration for the single ride is: {single_duration.iloc[0,0]} minutes"
-    )
-
-    # Batch prediction
-    batch_rides = [
-        {"PULocationID": "10", "DOLocationID": "50", "trip_distance": 40},
-        {"PULocationID": "20", "DOLocationID": "60", "trip_distance": 30},
-    ]
-    batch_durations = predictor.predict(batch_rides)
-    print(
-        f"The predicted durations for the batch rides are: \n{batch_durations['predictions']} minutes each"
-    )
-
-    # Batch prediction on another dataset
-    input_file = f"https://d37ci6vzurychx.cloudfront.net/trip-data/{taxi_type}_tripdata_{year:04d}-{month:02d}.parquet"
-    output_file = f"output/{taxi_type}/{year:04d}-{month:02d}.parquet"
-
-    output_df = predictor.process_and_save(input_file, output_file, model_version)
-    print(output_df.head(3))
-
-    # Upload to GCS
-    blob_name = f"data/predictions/{output_file.split('/')[-1]}"  # Specify your desired blob name in GCS
-    upload_to_gcs(output_file, blob_name, bucket_name)
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Train Taxi Ride Predictor")
-    parser.add_argument("--mode", choices=["train", "predict", "both"], default="train")
     parser.add_argument("--train_file", default="data/green_tripdata_2021-01.parquet")
     parser.add_argument("--val_file", default="data/green_tripdata_2021-02.parquet")
     parser.add_argument("--target", default="duration")
@@ -97,17 +56,4 @@ if __name__ == "__main__":
 
     predictor = TaxiRidePredictor(params=params)
 
-    # Call the training function if mode is 'train' or 'both'
-    if args.mode in ["train", "both"]:
-        run_training(args.train_file, args.val_file, args.target, params)
-
-    # Call the prediction function if mode is 'predict' or 'both'
-    if args.mode in ["predict", "both"]:
-        run_prediction(
-            predictor,
-            args.year,
-            args.month,
-            args.taxi_type,
-            args.model_version,
-            args.bucket_name,
-        )
+    run_training(args.train_file, args.val_file, args.target, params)
